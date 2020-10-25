4347d7d81275789b05bdb44f79a3a2944137f00b
billowycloud <bjkim0228@naver.com>
2020-10-25 15:18:01 +0900

    add logger

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-import rootReducer from "./modules";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './modules';
+import { createLogger } from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
+const logger = createLogger();
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+  composeWithDevTools(applyMiddleware(logger))
 );
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
